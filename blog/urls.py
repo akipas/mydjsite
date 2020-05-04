b64d2ef27c4920e3525ece599ae36da1e58fe11b
akipas <akimanapascaline@gmail.com>
2020-05-04 15:37:37 +0200

    creation des vues et urls du blog

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.accueil, name='accueil'),
+    path('article/<int:id>', views.lire, name='lire')
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import Http404
+from blog.models import Article
 
 # Create your views here.
+def accueil(request):
+    """Afficher tous les articles"""
+     articles = Article.objects.all()
+     return render(request, 'blog/accueil.html', {'derniers_articles':articles})
+
+def lire(request, id):
+    """afficher l'article en entier"""
+    pass
+ 
