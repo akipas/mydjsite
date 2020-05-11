e6f65ca3383de6a4e8b8691278426b870c9d3bfe
akipas <akimanapascaline@gmail.com>
2020-05-11 13:35:37 +0200

    ajout dun exemple de template

--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,8 @@ from blog.models import Article
 # Create your views here.
 def accueil(request):
     """Afficher tous les articles"""
-     articles = Article.objects.all()
-     return render(request, 'blog/accueil.html', {'derniers_articles':articles})
+    articles = Article.objects.all()
+    return render(request, 'blog/accueil.html', {'derniers_articles':articles})
 
 def lire(request, id):
     """afficher l'article en entier"""
