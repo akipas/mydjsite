bf247ba053128d897325b821c90751605f2388b6
akipas <akimanapascaline@gmail.com>
2020-05-13 15:34:17 +0200

    vue du formulaire de contact et correction de son modele

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    sujet = forms.CharFields(max_length=100)
+    sujet = forms.CharField(max_length=100)
     message = forms.CharField(widget=forms.Textarea)
-    envoyeur = forms.EmailField(label=""Votre adresse e-mail)
+    envoyeur = forms.EmailField(label="Votre adresse e-mail")
     renvoi = forms.BooleanField(help_text="cochez si vous souhaitez recevoir une copie du mail envoye.",required=False)
     
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from blog.models import Article
+from .forms import ContactForm
 
 # Create your views here.
 def accueil(request):
@@ -13,3 +14,19 @@ def lire(request, id, slug):
     article = get_object_or_404(Article, id=id, slug=slug)
     return render(request, 'blog/lire.html', {'article':article})
  
+def contact(request):
+    """costruction du formulaire avec les donnees de l'utilisateur ou vide si l'utilisateur accede pour la premiere fois"""
+    form = ContactForm(request.POST or None)
+    #verification des donnees envoyees
+    #le methode envoie False s'il n'ya pas de donnees dans le formulaire
+    #ou qu'il contien des erreurs
+    if form.is_valid():
+        sujet = form.cleaned_data['sujet']
+        message = form.cleaned_data['message']
+        envoyeur = forrm.claened_data['renvoi']
+
+        #envoi de l'e-mail grace aux donnees que nous venons de recuperer
+        envoi = True
+
+    #affichage de la page du formulaire
+    return(render,'blog/contact.html', locals)
