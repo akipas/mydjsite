261180f5d852a3f86750cd2034fa9795f191479c
akipas <akimanapascaline@gmail.com>
2020-05-13 15:39:05 +0200

    ajout de Url vers la page du formulaire

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.accueil, name='accueil'),
     path('article/<int:id>-<slug:slug>', views.lire, name='lire'),
+    path('contact',views.contact, name='contact'),
 
 ]
