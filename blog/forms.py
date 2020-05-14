deabe06e69362cbad0b6ad6b1844c1b4186046b5
akipas <akimanapascaline@gmail.com>
2020-05-14 10:57:39 +0200

    ajout d'une regle de validation du formulaire

--- blog/forms.py
+++ blog/forms.py
@@ -5,4 +5,24 @@ class ContactForm(forms.Form):
     message = forms.CharField(widget=forms.Textarea)
     envoyeur = forms.EmailField(label="Votre adresse e-mail")
     renvoi = forms.BooleanField(help_text="cochez si vous souhaitez recevoir une copie du mail envoye.",required=False)
-    
+
+    """def clean_message(self):
+        message = self.cleaned_data['message']
+        if "betail" or "ferme" in message:
+            raise forms.ValidationError("nous ne parlons pas de betail ou ferme ici")
+        #renvoyer le contenu du champ traite
+        return message
+    """
+    def clean(self):
+        cleaned_data = super(ContactForm, self).clean()
+        sujet = cleaned_data.get('sujet')
+        message = cleaned_data.get('message')
+
+        if sujet and message: #verifions d'abord si le sujet et le message sont valide
+            if "betail" in sujet and "betail" in message:
+                self.add_error("sujet",
+                    "Nous ne parlons pas de sujet a propos de betail ici"
+
+                )
+
+        return cleaned_data
