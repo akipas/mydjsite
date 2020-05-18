9e685b30eb6e87df62b71de90c1e42af351e58ed
akipas <akimanapascaline@gmail.com>
2020-05-18 11:54:06 +0200

    mise a jour des fichiers urls et settings global pour l'affichage de l'image du fiche d'inscription

--- mydjsite/urls.py
+++ mydjsite/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('blog/', include('blog.urls')),
@@ -22,4 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     
     
+    
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
